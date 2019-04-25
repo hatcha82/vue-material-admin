caaaaddbeadd14098ac0211b5718df84404bb857
Changbeom Ha <hatcha82@gmail.com>
2019-04-25 15:33:46 +0900

    lint error fixed

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -7,12 +7,32 @@
             <v-card class="elevation-1 pa-3">
               <v-card-text>
                 <div class="layout column align-center">
-                  <img src="/static/m.png" alt="Vue Material Admin" width="120" height="120">
-                  <h1 class="flex my-4 primary--text">Material Admin Template</h1>
-                </div>                
+                  <img
+                    src="/static/m.png"
+                    alt="Vue Material Admin"
+                    width="120"
+                    height="120"
+                  />
+                  <h1 class="flex my-4 primary--text">
+                    Material Admin Template
+                  </h1>
+                </div>
                 <v-form>
-                  <v-text-field append-icon="person" name="login" label="Login" type="text" v-model="model.username"></v-text-field>
-                  <v-text-field append-icon="lock" name="password" label="Password" id="password" type="password" v-model="model.password"></v-text-field>
+                  <v-text-field
+                    append-icon="person"
+                    name="login"
+                    label="Login"
+                    type="text"
+                    v-model="model.username"
+                  ></v-text-field>
+                  <v-text-field
+                    append-icon="lock"
+                    name="password"
+                    label="Password"
+                    id="password"
+                    type="password"
+                    v-model="model.password"
+                  ></v-text-field>
                 </v-form>
               </v-card-text>
               <v-card-actions>
@@ -26,7 +46,9 @@
                   <v-icon color="light-blue">fa fa-twitter fa-lg</v-icon>
                 </v-btn>
                 <v-spacer></v-spacer>
-                <v-btn block color="primary" @click="login" :loading="loading">Login</v-btn>
+                <v-btn block color="primary" @click="login" :loading="loading"
+                  >Login</v-btn
+                >
               </v-card-actions>
             </v-card>
           </v-flex>
@@ -41,30 +63,29 @@ export default {
   data: () => ({
     loading: false,
     model: {
-      username: 'admin@isockde.com',
-      password: 'password'
+      username: "admin@isockde.com",
+      password: "password"
     }
   }),
 
   methods: {
-    login () {
+    login() {
       this.loading = true;
       setTimeout(() => {
-        this.$router.push('/dashboard');
+        this.$router.push("/dashboard");
       }, 1000);
     }
   }
-
 };
 </script>
 <style scoped lang="css">
-  #login {
-    height: 50%;
-    width: 100%;
-    position: absolute;
-    top: 0;
-    left: 0;
-    content: "";
-    z-index: 0;
-  }
+#login {
+  height: 50%;
+  width: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  content: "";
+  z-index: 0;
+}
 </style>
